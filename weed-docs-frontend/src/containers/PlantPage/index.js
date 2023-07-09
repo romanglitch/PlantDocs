e8a6af941bbb340df37f30d01eb74922ab4e366e
Roman Glitch <tagshowpictures@gmail.com>
2023-07-09 03:25:25 +0300

    small updates

--- weed-docs-frontend/src/containers/PlantPage/index.js
+++ weed-docs-frontend/src/containers/PlantPage/index.js
@@ -13,6 +13,11 @@ import './styles.css';
 const PlantPage = () => {
     const [error, setError] = useState(null);
     const [plantsPage, setPlantsPage] = useState([]);
+    const [isActive, setActive] = useState("false");
+
+    const handleToggle = () => {
+        setActive(!isActive);
+    };
 
     let {id} = useParams();
 
@@ -27,8 +32,6 @@ const PlantPage = () => {
         // Print errors if any
         return <div>An error occured: {error.message}</div>;
     }
-
-    console.log(plantsPage.categories)
 
     return (
         <div className="plant-page">
@@ -50,46 +53,51 @@ const PlantPage = () => {
             <div className="plant-page__row plant-page__content">
                 Контент растения: {plantsPage.Content}
             </div>
-            <div className="plant-page__row plant-page__weeks">
-                {plantsPage.weeks ? (
-                    plantsPage.weeks.map((data, index) => (
-                        <div className="week" data-id={data.id} key={data.id}>
-                            <div className="week__title">
-                                Неделя: {index + 1}
-                            </div>
-                            <div className="week__days">
-                                {
-                                    data.days.map((days_data, index) => (
-                                        <div className="day" data-id={days_data.id} key={days_data.id}>
-                                            <div className="day__title">
-                                                ({index + 1} день)
-                                            </div>
-                                            <div className="day__date">
-                                                {days_data.date}
-                                            </div>
-                                            <div className="day__tags">
-                                                {days_data.tags.data ? (
-                                                    days_data.tags.data.map((tags_data) => (
-                                                        <div className="tag" data-id={tags_data.id} key={tags_data.id}>
-                                                            <div className="tag__name">
-                                                                {tags_data.attributes.name}
-                                                            </div>
-                                                            <div className="tag__icon">
-                                                                <img src={'http://localhost:1337' + tags_data.attributes.icon.data.attributes.url} alt={tags_data.attributes.name} />
-                                                            </div>
+            <button className="plant-page__btn" onClick={handleToggle}>
+                {!isActive ? "Свернуть недели" : "Открыть недели"}
+            </button>
+            {!isActive ? (
+                <div className="plant-page__row plant-page__weeks">
+                    {plantsPage.weeks ? (
+                        plantsPage.weeks.map((data, index) => (
+                            <div className="week" data-id={data.id} key={data.id}>
+                                <div className="week__title">
+                                    Неделя: {index + 1}
+                                </div>
+                                <div className="week__days">
+                                    {
+                                        data.days.map((days_data, index) => (
+                                            <div className="day" data-id={days_data.id} key={days_data.id}>
+                                                <div className="day__title">
+                                                    ({index + 1} день)
+                                                </div>
+                                                <div className="day__date">
+                                                    {days_data.date}
+                                                </div>
+                                                <div className="day__tags">
+                                                    {days_data.tags.data ? (
+                                                        days_data.tags.data.map((tags_data) => (
+                                                            <div className="tag" data-id={tags_data.id} key={tags_data.id}>
+                                                                <div className="tag__name">
+                                                                    {tags_data.attributes.name}
+                                                                </div>
+                                                                <div className="tag__icon">
+                                                                    <img src={'http://localhost:1337' + tags_data.attributes.icon.data.attributes.url} alt={tags_data.attributes.name} />
+                                                                </div>
 
-                                                        </div>
-                                                    ))
-                                                ) : false}
+                                                            </div>
+                                                        ))
+                                                    ) : false}
+                                                </div>
                                             </div>
-                                        </div>
-                                    ))
-                                }
+                                        ))
+                                    }
+                                </div>
                             </div>
-                        </div>
-                    ))
-                ) : false}
-            </div>
+                        ))
+                    ) : false}
+                </div>
+            ) : false}
             <div className="plant-page__row plant-page__footer">
                 <Link className="plant-page__link" to="/">Go home</Link>
             </div>
