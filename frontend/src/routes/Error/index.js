fc71a1b9994c32d27df3818f71493115fb6db89c
Roman Glitch <tagshowpictures@gmail.com>
2023-07-31 22:05:27 +0300

    Error page is ok

--- frontend/src/routes/Error/index.js
+++ frontend/src/routes/Error/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import { Link } from "react-router-dom";
-import {Card} from "antd";
-
-// Components
-import AppCardTitle from "../../components/AppCardTitle";
+import { Button, Result, Card } from 'antd';
 
 // Styles
 import './styles.css'
 
 const Error = () => {
     return (
-        <Card className="app-card card-error" title={AppCardTitle('Ошибка')}>
-            Страница не найдена (<Link to={'/'}>Вернуться на главную</Link>)
+        <Card className="app-card card-error">
+            <Result
+                status="404"
+                title="Ну и что здесь ?...."
+                subTitle="Извините, но данной страницы не существует"
+                extra={<Button type="primary" href={'/'}>Вернуться на главную</Button>}
+            />
         </Card>
     );
 }
