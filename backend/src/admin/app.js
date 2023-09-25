d94f66093359ab84d99334247cb9b3b00b6e9ab2
Roman Glitch <tagshowpictures@gmail.com>
2023-09-25 17:42:43 +0300

    Small blobs in backend

--- backend/src/admin/app.js
+++ backend/src/admin/app.js
@@ -1,18 +1,20 @@
-// import favicon from './extensions/favicon-32x32.png';
+import favicon from './extensions/favicon-32x32.png';
 
 export default {
   config: {
     locales: ['ru'],
-    // head: {
-    //   favicon: favicon,
-    // }
+    head: {
+      favicon: favicon,
+    },
     translations: {
-      ru: {
-        "app.components.HomePage.welcome.again": "Панель управления PlantDocs ёпта",
-        "app.components.LeftMenu.navbrand.title": "Внатуре это PlantDocs"
+      ru: { // Возможно имеет смысл изменить экземпляр Strapi, дабы при переключении на англ. язык везде был PlantDocs и.т.д
+        "app.components.HomePage.welcome.again": "Панель управления PlantDocs",
+        "app.components.LeftMenu.navbrand.title": "PlantDocs",
+        "app.components.LeftMenu.plugins": "Модули"
       },
     },
     tutorials: false,
+    // При изменении экземпляра имеет смысл отключить обновления
     // notifications: {
     //   releases: false
     // },
