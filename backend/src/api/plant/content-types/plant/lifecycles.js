e70710fad8409bb8c390203ca1893c28d8d4d92e
Roman Glitch <tagshowpictures@gmail.com>
2023-11-28 19:02:20 +0300

    fix adding days (invalid date)

--- backend/src/api/plant/content-types/plant/lifecycles.js
+++ backend/src/api/plant/content-types/plant/lifecycles.js
@@ -2,44 +2,41 @@ module.exports = {
   afterCreate(event) {
     const { result, params } = event;
 
-    const getDate = (addDays) => {
-      let today = new Date(result.updatedAt);
-      let getDate = addDays ? today.getDate() + addDays : today.getDate();
-      let dd = String(getDate).padStart(2, '0');
-      let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-      let yyyy = today.getFullYear();
+    // Add one day to current date
+    const addOneDayToDate = (count) => {
+      const date = new Date();
 
-      today = yyyy + '-' + mm + '-' + dd;
+      count ? date.setDate(date.getDate() + count) : false
 
-      return today
+      return date
     }
 
-    // Add first week and first 7 days
+    // Add 1 week and 7 days
     strapi.entityService.update('api::plant.plant', result.id, {
       data: {
         weeks: [
           {
             days: [
               {
-                date: getDate()
+                date: addOneDayToDate()
               },
               {
-                date: getDate(1)
+                date: addOneDayToDate(1)
               },
               {
-                date: getDate(2)
+                date: addOneDayToDate(2)
               },
               {
-                date: getDate(3)
+                date: addOneDayToDate(3)
               },
               {
-                date: getDate(4)
+                date: addOneDayToDate(4)
               },
               {
-                date: getDate(5)
+                date: addOneDayToDate(5)
               },
               {
-                date: getDate(6)
+                date: addOneDayToDate(6)
               }
             ]
           }
